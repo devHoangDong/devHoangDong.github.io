bdd87eced246e5c0ae0af081f00f6ee91f6e53cb
dong-hoang <jetaimend@gmail.com>
2021-04-07 16:03:59 +0700

    update

--- myjs.js
+++ myjs.js
@@ -18,7 +18,4 @@ function typeWriter() {
 
 //paste this code under the head tag or in a separate js file.
 	// Wait for window load
-	$(window).load(function() {
-		// Animate loader off screen
-		$(".present-ext").fadeOut("slow");
-	});
+	
