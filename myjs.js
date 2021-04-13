c497530df15ae2b4b9213c110b5cddc8b27ea57e
dong-hoang <jetaimend@gmail.com>
2021-04-13 16:03:44 +0700

    update

--- myjs.js
+++ myjs.js
@@ -1,3 +1,4 @@
+// typewriter
 
 var i = 0;
 var speed = 50;
@@ -15,5 +16,50 @@ function typeWriter() {
     }
     setTimeout(typeWriter, speed);
 }
+// filter button
+myFilter('all');
+function myFilter(n) {
+    let j;
+    var x = document.getElementsByClassName('project');
+    if (n == 'all') n = '';
+    for (j = 0;j < x.length; j++) {
+        removeClass(x[j], 'show');
+        if (x[j].className.indexOf(n) > -1) {
+            addClass(x[j], 'show');
+        }
+    }
+}
+function removeClass(element, name) {
+    let j;
+    var arr1 = element.className.split(' ');
+    var arr2 = name.split(' ');
+    for (j=0;j<arr2.length;j++) {
+        while (arr1.indexOf(arr2[j]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[j]),1);
+        };
+    }
+    element.className= arr1.join(' ');
+}
+function addClass(element, name) {
+    let j;
+    var arr1 = element.className.split(' ');
+    var arr2 = name.split(' ');
+    for (j=0;j<arr2.length;j++) {
+        if (arr1.indexOf(arr2[j]) == -1) {
+            element.className += ' ' + arr2[j];
+        };
+    }
+}
+// show active button
 
+var btnContainer = document.getElementById("mybtn");
+var btns = btnContainer.getElementsByClassName("btn");
+console.log(btns);
+for (let j = 0; j < btns.length; j++) {
+  btns[j].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 	
